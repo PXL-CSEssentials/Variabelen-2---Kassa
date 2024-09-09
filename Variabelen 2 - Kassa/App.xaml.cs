6c4a9eb4e1760aadb9804f2361bbc3a91437e43b
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-09 17:17:19 +0200

    final

---
+++ Variabelen 2 - Kassa/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Variabelen_2___Kassa
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Variabelen 2 - Kassa/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oefening2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkNumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            const int getal = 97;
+            int ondernemingsnummer = int.Parse(VATNumberTextBox.Text);
+            // int ondernemingsnummer = Convert.ToInt32(TxtOndNr1.Text);
+            int resultaat = getal - (ondernemingsnummer % getal);
+
+            checkNumberTextBox.Text = Convert.ToString(resultaat);
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Declaratie variabelen.     
+            float prijs, teBetalen;
+            int aantal;
+
+            // De gebruiker geeft de prijs en het aantal stuks in.  
+            // prijs = Convert.ToSingle(TxtPrijs.Text); 
+            prijs = float.Parse(priceTextBox.Text);
+            aantal = int.Parse(amountTextBox.Text);
+
+            // Berekenen van het te betalen bedrag.     
+            teBetalen = prijs * aantal;
+
+            // Tonen van het te betalen bedrag.
+            amountToPayTextBox.Text = teBetalen.ToString();
+
+            clearButton.Focus();
+        }
+
+        public void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            amountTextBox.Text = string.Empty;
+            priceTextBox.Text = "";
+            amountToPayTextBox.Text = string.Empty;
+            VATNumberTextBox.Clear();
+            checkNumberTextBox.Clear();
+
+            priceTextBox.Focus();
+        }
+
+        public void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Formulier sluiten.       
+            Close();
+        }
+    }
+}
