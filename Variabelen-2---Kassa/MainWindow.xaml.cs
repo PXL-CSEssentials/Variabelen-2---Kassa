8bd593d5619c60a1e9416ae811a75bf20dd567ef
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-10 09:06:04 +0200

    Update namespace

--- Variabelen-2---Kassa/MainWindow.xaml.cs
+++ Variabelen-2---Kassa/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Oefening2
+namespace Variabelen_2___Kassa
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
